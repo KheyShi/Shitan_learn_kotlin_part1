03cfc5639886fe2fa9c44e07997a2a2c96e7fe86
KheyShi <89207382+KheyShi@users.noreply.github.com>
2022-08-26 18:47:52 +0800

    Add files via upload

---
+++ 1 Introduction to Kotlin/Lesson1.kt
@@ -0,0 +1,67 @@
+//1
+
+fun main() {
+  println("Hello, World!")
+}
+
+//2
+fun main(){
+  println("Coding is fun!")
+}
+
+//3
+fun main() {
+  println("Just keep swimming!")
+  print(15 * 4)
+
+}
+
+//4
+
+fun main() {
+  println("11/2 - AU military goes to war against local emus.")
+  println("11/4 - Ambush planned by military fails.")
+  println("11/8 - AU military withdraws.")
+
+}
+
+//5
+
+fun main() {
+  println("Howdy, partner!") 
+}
+
+//Code prints "Howdy, partner!" to the terminal.
+
+/* I learned a lot
+yeaaah
+hello */
+
+//6
+
+fun main() {
+  println("An error a day keeps the programmer away.")
+}
+
+//7
+fun main() 
+{
+  //        Organize the code below
+    println("Down by the bay")
+    println("Where the watermelons grow,")
+    println("Back to my home")
+    println("I dare not go.");
+    println("For if I do");
+    println("My mother will say")
+    println("Did you ever use an app that taught you how to snap?")
+    println("Down by the bay!")
+}
+
+//8
+
+fun main(){
+  print(2022-2001)
+  println("heydo")
+
+  //prints
+}
